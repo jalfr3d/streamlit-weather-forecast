b662e161adc3af8f0360185146c2fd599b5929fc
Jorge <alfred.jorge@gmail.com>
2023-01-10 19:40:55 -0300

    Create backend and filtering the data from the API

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import plotly.express as px
+from backend import get_data
+
 
 st.title("Weather Forecast for the Next Days")
 place = st.text_input("Place: ")
@@ -9,14 +11,7 @@ option = st.selectbox("Select data to view",
                       ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
-
-def get_data(days):
-    dates = ["2022-25-10", "2022-26-10"]
-    temperatures = [10, 11]
-    temperatures = [days * i for i in temperatures]
-    return dates, temperatures
-
-
+data = get_data(place, days, option)
 d, t = get_data(days)
 
 figure = px.line(x=d, y=t, labels={"x": "Dates", "y": "Temperature (C)"})
